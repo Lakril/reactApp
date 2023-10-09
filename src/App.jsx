347a5530efe2e2e512b644bb1feff73d85bae1ed
Jackson Rico <jacksonrico@gmail.com>
2023-10-09 15:56:30 -0500

    feat: new app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import LeftBar from './components/leftbar'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+
+  return (
+    <>
+      <LeftBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/leftbar.jsx
@@ -0,0 +1,21 @@
+
+import style from "./leftbar.css" 
+
+export const LeftBar = () => {
+
+    return (
+        <>
+            <div className={style.container}>
+
+                dfslkdfjsldkj
+            </div>
+            <div>
+                otro contenedor
+            </div>         </>
+    )
+
+
+}
+
+
+export default LeftBar
